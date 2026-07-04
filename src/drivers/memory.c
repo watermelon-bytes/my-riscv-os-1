ff4f8bc9258cc72c57e24d61517a19d71c62ff95
watermelon-bytes <water@melons.io>
2026-07-04 12:08:12 +0300

    add debug function for logging detected memory

--- src/drivers/memory.c
+++ src/drivers/memory.c
@@ -1,10 +1,17 @@
 #include <libfdt.h>
+#include <klibc/printf.h>
 #include <init_devices.h>
 #include <types.h>
 #include <drivers/memory.h>
 
+struct space {
+    uintptr_t beginning_physicaddr;
+    size_t space_size;
+} ram_regions[MAX_MEM_REGIONS] = {};
+
 // TODO: replace raw `int` with `enum` or something
 int extract_ram_region_info(const void* device_tree, int node_offset) {
+    printf("extract_ram_region_info called\n");
     int length;
     // properties are constructed using 32-bit "cells"
     const uint32_t* ptr = fdt_getprop(device_tree, node_offset, "reg", &length);
@@ -26,5 +33,15 @@ int register_memory_regions(const uint32_t* first_cell, size_t len) {
             fetch_native_pointer(&(buf->beginning_physicaddr), first_cell);
         first_cell = fetch_size_field(&(buf->space_size), first_cell);
     }
+    printf("register_memory_regions: success!\n");
     return 0;
 }
+
+#ifdef DEBUG
+void log_detected_memory() {
+    for (uint32_t i = 0; i < MAX_MEM_REGIONS; ++i) {
+        printf("detected RAM area no. %i: starts at %x, size = %lu\n", i + 1,
+               ram_regions[i].beginning_physicaddr, ram_regions[i].space_size);
+    }
+}
+#endif /* ifdef DEBUG */
--- src/drivers/memory.h
+++ src/drivers/memory.h
@@ -6,11 +6,6 @@
 
 #define MAX_MEM_REGIONS 16
 
-struct {
-    uintptr_t beginning_physicaddr;
-    size_t space_size;
-} ram_regions[MAX_MEM_REGIONS] = {};
-
 _Static_assert(
     sizeof(void*) == sizeof(uintptr_t),
     "Impossible error: sizeof(uintptr_t) != sizeof(void*)");  // who knows
@@ -20,3 +15,7 @@ int register_memory_regions(const uint32_t* first_cell, size_t len);
 enum { SUCCESS, TOO_MANY_REGIONS };
 
 int extract_ram_region_info(const void* device_tree, int node_offset);
+
+#ifdef DEBUG
+void log_detected_memory();
+#endif
