ada4e22d1e3b0ffaae3266a513743d04e7e7c265
watermelon-bytes <water@melons.io>
2026-07-05 19:49:45 +0300

    add `types.h` header for custom type aliasing

---
+++ src/types.h
@@ -0,0 +1,17 @@
+#pragma once
+#include <stddef.h>
+#include <stdint.h>
+
+typedef unsigned int uint;
+typedef uint16_t u16;
+typedef int16_t i16;
+typedef uint32_t u32;
+typedef uint64_t u64;
+typedef int64_t i64;
+typedef int32_t i32;
+typedef uint8_t u8;
+typedef int8_t i8;
+typedef uintptr_t register_t;
+typedef unsigned char u_char;
+
+#define countof(array) (sizeof(array) / sizeof(array[0]))
