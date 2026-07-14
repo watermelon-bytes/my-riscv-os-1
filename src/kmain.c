9406256fb496fe6dbd9aea11ae38ee7638f7b2d5
watermelon-bytes <water@melons.io>
2026-07-14 13:17:24 +0300

    call `ensure_extensions_present()` at the very beginning of `kmain`

--- src/kmain.c
+++ src/kmain.c
@@ -2,8 +2,10 @@
 #include <init_devices.h>
 #include <klibc/panic.h>
 #include <klibc/printf.h>
+#include <riscv/extension_check.h>
 
 void kmain(int hardt_id, void* device_tree) {
+    ensure_extensions_present();
     printf("Booting on on hardware thread %i\n", hardt_id);
     if (!check_device_tree(device_tree)) {
         KERNEL_PANIC("invalid device tree was passed");
