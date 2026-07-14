764b2de5dd4c2f47e389027e26acc56d7bd46433
watermelon-bytes <water@melons.io>
2026-07-14 12:06:32 +0300

    riscv: implement mandatory ISA extension validation on boot refactor: name extensions more consistently

--- src/riscv/extension_check.c
+++ src/riscv/extension_check.c
@@ -1,7 +1,29 @@
+#include <klibc/panic.h>
+#include <printf.h>
 #include <riscv/csr_operations.h>
 #include <riscv/extension_check.h>
+#include <utils.h>
+
+// NOTE: Consider moving this array definition to some higher-level code. This
+// code has to just provide convenient function-wrappers for machine information
+// discovery, not decide which extensions to require.
+enum riscv_extensions required_riscv_extension[] = {
+    RISCV_EXT_MUL_DIV,
+    RISCV_EXT_USER_MODE,
+};
 
 bool is_extension_implemented(enum riscv_extensions ext) {
     u32 misa = READ_CSR(misa);
     return (misa & ext);
+}
+
+void ensure_extensions_present() {
+    for (uint i = 0; i < countof(required_riscv_extension); ++i) {
+        // TODO: Print more detailed info on what exactly extension is missing
+        // but required by kernel.
+        if (is_extension_implemented(required_riscv_extension[i]) == false) {
+            k_panic("a required extension is not implemented\n");
+        }
+    }
+    printf("[OK] All needed extensions present\n");
 }
--- src/riscv/extension_check.h
+++ src/riscv/extension_check.h
@@ -4,18 +4,18 @@
 
 // clang-format off
 enum riscv_extensions : u32 {
-    EXT_ATOMIC              = 1u << 0,
-    EXT_BIT_MANIPULATION    = 1u << 1,
-    EXT_COMPRESSED          = 1u << 2,
-    EXT_DOUBLE_PREC_FPU     = 1u << 3,
-    EXT_LESS_REGISTERS      = 1u << 4,
-    EXT_FLOATING_POINT      = 1u << 5,
-    EXT_MULTIPLY_N_DIVIDE   = 1u << 12,
-    EXT_QUADRA_PREC_FPU     = 1u << 16,
-    EXT_SUPERVISOR_MODE     = 1u << 18,
-    EXT_USER_MODE           = 1u << 20,
-    EXT_VECTOR              = 1u << 21,
-    EXT_NON_STANDARD        = 1u << 23
+    RISCV_EXT_ATOMIC              = 1u << 0,
+    RISCV_EXT_BIT_MANIPULATION    = 1u << 1,
+    RISCV_EXT_COMPRESSED          = 1u << 2,
+    RISCV_EXT_DOUBLE              = 1u << 3,
+    RISCV_EXT_LESS_REGS           = 1u << 4,
+    RISCV_EXT_FLOAT               = 1u << 5,
+    RISCV_EXT_MUL_DIV             = 1u << 12,
+    RISCV_EXT_QUAD                = 1u << 16,
+    RISCV_EXT_SUPERVISOR_MODE     = 1u << 18,
+    RISCV_EXT_USER_MODE           = 1u << 20,
+    RISCV_EXT_VECTOR              = 1u << 21,
+    RISCV_EXT_NON_STANDARD        = 1u << 23
 };
 // clang-format on
 
@@ -23,3 +23,7 @@ enum riscv_extensions : u32 {
 // returns non-zero value Returns 0 if the specified extension is not present on
 // the current machine
 bool is_extension_implemented(enum riscv_extensions);
+
+// Calls kernel panic the CPU if `is_extension_implemented` returns false on
+// any of `required_riscv_extension`s
+void ensure_extensions_present();
