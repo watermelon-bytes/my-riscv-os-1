5fefd96f57353688f8f8b81d7835c061f62f3b75
watermelon-bytes <water@melons.io>
2026-07-14 13:32:13 +0300

    remove `k_panic`

--- src/klibc/panic.c
+++ src/klibc/panic.c
@@ -7,11 +7,3 @@ loop:
     __asm__ __volatile__("wfi; \n");
     goto loop;
 }
-
-#define KERNEL_PANIC(...)            \
-    ({                               \
-        printf("[PANIC] halting: "); \
-        printf(__VA_ARGS__);         \
-        _putchar('\n');              \
-        halt();                      \
-    })
