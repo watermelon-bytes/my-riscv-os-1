b0f3c630b2de301f89483f84ac142bdd8d899a0e
watermelon-bytes <water@melons.io>
2026-07-15 09:15:06 +0300

    format `csr_operations.h`

--- src/riscv/csr_operations.h
+++ src/riscv/csr_operations.h
@@ -1,15 +1,16 @@
 #pragma once
 #include <types.h>
+
 #if defined(__GNUC__) || defined(__clang__)
-#define READ_CSR(reg)                                    \
-    ({                                                   \
-        register_t _tmp;                                 \
-        __asm__ volatile("csrr %0, " #reg : "=r"(_tmp)); \
-        _tmp;                                            \
-    });
-#define WRITE_CSR(reg, val)                                \
-    ({                                                     \
-        register_t _tmp = (val);                           \
-        __asm__ volatile("csrw " #reg ", %0" ::"r"(_tmp)); \
-    });
+    #define READ_CSR(reg)                                    \
+        ({                                                   \
+            register_t _tmp;                                 \
+            __asm__ volatile("csrr %0, " #reg : "=r"(_tmp)); \
+            _tmp;                                            \
+        });
+    #define WRITE_CSR(reg, val)                                \
+        ({                                                     \
+            register_t _tmp = (val);                           \
+            __asm__ volatile("csrw " #reg ", %0" ::"r"(_tmp)); \
+        });
 #endif
