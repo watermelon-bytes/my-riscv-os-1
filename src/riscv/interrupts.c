88975ef99aa68169b35e205353309a4fc2c29193
watermelon-bytes <water@melons.io>
2026-07-17 17:04:25 +0300

    implement basic interrupt handler; to be developed better later

--- src/riscv/interrupts.c
+++ src/riscv/interrupts.c
@@ -1,7 +1,6 @@
-#include "interrupt_controller.h"
-
+#include <klibc/panic.h>
 #include <riscv/csr_operations.h>
-
+#include <riscv/interrupts.h>
 void disable_interrupts() {
     __auto_type curr_mstatus_value = READ_CSR(mstatus);
     WRITE_CSR(mstatus, curr_mstatus_value & ~MSTATUS_MIE);
@@ -10,4 +9,20 @@ void disable_interrupts() {
 void enable_interrupts() {
     __auto_type curr_mstatus_value = READ_CSR(mstatus);
     WRITE_CSR(mstatus, curr_mstatus_value | MSTATUS_MIE);
+}
+
+__attribute__((used, noreturn)) void handle() {
+    while (true) {
+        asm volatile("wfi;");
+    }
+    __builtin_unreachable();
+}
+
+void setup_interrupt_handler() {
+    asm volatile(
+        "csrw mideleg, zero;"
+        "csrw medeleg, zero;");
+    ASSERT((uintptr_t)&handle % 4 == 0);
+    __auto_type handler_address = (uintptr_t)&handle;
+    WRITE_CSR(mtvec, handler_address | MTVEC_MODE_DIRECT);
 }
--- src/riscv/interrupts.h
+++ src/riscv/interrupts.h
@@ -18,3 +18,19 @@ void enable_interrupts();
 enum {
     MSTATUS_MIE = 1u << 3,
 };
+
+enum mtvec_mode_field { MTVEC_MODE_DIRECT, MTVEC_MODE_VECTORED };
+
+union mtrap_vector {
+    register_t _raw_value;
+    struct {
+        register_t mode : 2;
+        register_t base : sizeof(uintptr_t) - 2;
+    };
+};
+
+_Static_assert(sizeof(union mtrap_vector) == sizeof(register_t),
+               "Poorly formatted `mtvec` fields (in source file " __FILE_NAME__
+               ")\n");
+
+void setup_interrupt_handler();
