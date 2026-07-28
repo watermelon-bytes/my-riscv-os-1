7e12fe9fe555a75d8d077b06b3921b978fe812b0
watermelon-bytes <water@melons.io>
2026-07-28 10:09:38 +0300

    feat: implement `bitmap` as a separate interface

---
+++ src/klibc/bitmap.c
@@ -0,0 +1,60 @@
+// Common interface for bitmaps
+#include <bitwise_utils.h>
+#include <klibc/bitmap.h>
+#include <klibc/panic.h>
+#include <klibc/utils.h>
+#include <limits.h>
+
+void bitmap_init(struct bitmap* map, register_t slots[], size_t size) {
+    ASSERT(slots != NULL);
+    if (slots == NULL || size == 0) {
+        return;
+    }
+    map->total = map->free_slots_count = size * CHAR_BIT * sizeof(slots[0]);
+    map->last_freed = 0;
+    word_aligned_memset(slots, 0, size);
+}
+
+int bitmap_allocate_slot(struct bitmap* map) {
+    ASSERT(map != NULL);
+    for (size_t i = 0; i < map->total; ++i) {
+        if (map->slots_ptr[i] == UINTPTR_MAX) {
+            continue;
+        } else {
+            const uint offset = count_trailing_zeroes(map->slots_ptr[i]);
+            map->slots_ptr[i] |= 1 << offset;
+            map->free_slots_count--;
+            return WORD_SIZE * i + offset;
+        }
+    }
+    return -1;
+}
+
+void bitmap_free_slot(struct bitmap* map, uint slot) {
+    ASSERT(map->total > slot);
+    return;
+}
+
+void mark_as_used(struct bitmap* bitmap, uint first_slot,
+                  size_t slots_to_mark) {
+    ASSERT(bitmap != NULL);
+    ASSERT(bitmap->slots_ptr != NULL);
+    uint word_index = first_slot / WORD_SIZE, rem = first_slot % WORD_SIZE;
+    if (rem) {
+        bitmap->slots_ptr[word_index] |=
+            UINTPTR_MAX >> (WORD_SIZE - first_slot);
+        word_index++;
+        slots_to_mark -= rem;
+    }
+
+    while (slots_to_mark > WORD_SIZE) {
+        bitmap->slots_ptr[word_index] = UINTPTR_MAX;
+        slots_to_mark -= WORD_SIZE;
+        ++word_index;
+    }
+
+    if (slots_to_mark > 0) {
+        bitmap->slots_ptr[word_index] |= UINTPTR_MAX
+                                         << (WORD_SIZE - slots_to_mark);
+    }
+}
---
+++ src/klibc/bitmap.h
@@ -0,0 +1,25 @@
+// Common interface for bitmaps
+#pragma once
+#include <types.h>
+
+struct bitmap {
+    int total;
+    uint free_slots_count;
+    uint last_freed;
+    register_t* slots_ptr;
+};
+
+/*
+ * Initializes the bitmap pointed to by @map with all zeroes
+ * @slots Word-aligned buffer that belongs to the bitmap from now on.
+ * @size Size of slots buffer in words (i.e. if buffer length = 64 bytes, and
+ * word size is 4 bytes, then size must be 16)
+ */
+void bitmap_init(struct bitmap*, register_t slots[], size_t size);
+
+// Returns slot no.
+int bitmap_allocate_slot(struct bitmap*);
+
+void bitmap_free_slot(struct bitmap*, uint slot);
+
+void mark_as_used(struct bitmap*, uint first_slot, size_t slots_to_mark);
