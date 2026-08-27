86ad6fbce9b176de9609960a63f74a9abae7fa75
watermelon-bytes <water@melons.io>
2026-08-27 20:07:51 +0300

    refactor(clint): add function descriptions in header file `clint.h`

--- src/riscv/clint/clint.c
+++ src/riscv/clint/clint.c
@@ -10,7 +10,7 @@ constexpr __auto_type CLINE_MSIP_OFFSET = 0x0u;
 constexpr __auto_type CLINT_MTIMECMP_OFFSET = 0x4000u;
 constexpr __auto_type CLINT_MTIME_OFFSET = 0xBFF8u;
 #else
-    #define CLINE_MSIP_OFFSET 0x0u
+    #define CLINT_MSIP_OFFSET 0x0u
     #define CLINT_MTIMECMP_OFFSET 0x4000u
     #define CLINT_MTIME_OFFSET 0xBFF8u
 #endif
@@ -24,23 +24,22 @@ int discover_clint(const void* tree) {
         if (parse_reg(tree, clint_node, &clint_addr, &size) != 0) {
             return -2;
         }
-        printf("CLINT base address = %p\n", clint_addr);
+        printf("[OK] CLINT base address = %p\n", clint_addr);
         return 0;
     }
     return -1;
 }
 
-u64* clint_get_time() { return (u64*)(clint_addr + CLINT_MTIME_OFFSET); }
+static u64* clint_get_time() { return (u64*)(clint_addr + CLINT_MTIME_OFFSET); }
 
 // Returns mtimecmp for the current hart.
-u64* clint_get_mtimecmp() {
+static u64* clint_get_mtimecmp() {
     __auto_type mtimecmp_base = (u64*)(clint_addr + CLINT_MTIMECMP_OFFSET);
     return &mtimecmp_base[READ_CSR(mhartid)];
 }
 
-// Since struct riscv_timer fits into 2 registers, we may return it as value
-// (will be placed in a0-a1, accoring to the RISC-V calling convention)
 struct riscv_timer clint_init_timer() {
-    return (struct riscv_timer){.mtime = clint_get_time(),
-                                .mtimecmp = clint_get_mtimecmp()};
+    // Requires no stack slots for return value as it fits into a0-a1
+    return (struct riscv_timer){.mtime = (reg_t*)clint_get_time(),
+                                .mtimecmp = (reg_t*)clint_get_mtimecmp()};
 }
--- src/riscv/clint/clint.h
+++ src/riscv/clint/clint.h
@@ -1,6 +1,18 @@
 #pragma once
 #include <riscv/interrupts/interrupts.h>
 
-int discover_clint(const void* tree);
+/*
+ * @brief Tries to fetch a CLINT node in the given Device Tree
+ * @return
+ * -1 if the CLINT node is not present
+ * -2 if the CLINT node was found but could not be parsed
+ * 0 if CLINT device registers were discovered successfully
+ *
+ */
+int discover_clint(const void* device_tree);
 
+/*
+ * @brief Fills a structure `riscv_timer`,  based on the CLINT base address,
+ * and returns it.
+ */
 struct riscv_timer clint_init_timer();
