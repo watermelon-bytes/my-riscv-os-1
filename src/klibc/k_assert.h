93d8075e083f74dff559d6b0f746c9851afacdce
watermelon-bytes <water@melons.io>
2026-09-14 15:45:41 +0300

    fix: save build time by using `extern` declaration instead of `#include`
    Earlier could not compile as `DEBUG_ASSERT` has been moved to separate header

--- src/klibc/k_assert.h
+++ src/klibc/k_assert.h
@@ -1,23 +1,23 @@
 #pragma once
-
+extern int printf_(const char* format, ...);
 extern void halt();
 #ifndef NDEBUG
-    #define DEBUG_ASSERT(condition)                              \
-        ({                                                       \
-            if (!(condition)) {                                  \
-                printf("-----------------------------------\n"); \
-                printf("[HALT] Assertion failed: " #condition    \
-                       " evaluates to 'false'\n"                 \
-                       "at %s: in function %s, line %d\n",       \
-                       __FILE__, __PRETTY_FUNCTION__, __LINE__); \
-                halt();                                          \
-            }                                                    \
+    #define DEBUG_ASSERT(condition)                               \
+        ({                                                        \
+            if (!(condition)) {                                   \
+                printf_("-----------------------------------\n"); \
+                printf_("[HALT] Assertion failed: " #condition    \
+                        " evaluates to 'false'\n"                 \
+                        "at %s: in function %s, line %d\n",       \
+                        __FILE__, __PRETTY_FUNCTION__, __LINE__); \
+                halt();                                           \
+            }                                                     \
         })
 
     #define DEBUG_ASSERT_WITH_MSG(condition, ...) \
         ({                                        \
             if (!(condition)) {                   \
-                printf(__VA_ARGS__);              \
+                printf_(__VA_ARGS__);             \
                 halt();                           \
             }                                     \
         })
