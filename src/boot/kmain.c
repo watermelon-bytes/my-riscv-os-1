211a589669468bcc36b964c13f64b22a1dd76f0f
watermelon-bytes <water@melons.io>
2026-09-16 23:08:01 +0300

    `kmain`: `halt()` if hardware thread ID isn't 0

--- src/boot/kmain.c
+++ src/boot/kmain.c
@@ -25,25 +25,29 @@ const u32 NECESSARY_RISCV_EXTENSIONS = RISCV_OPTIONAL_ZBB |
 // clang-format on
 
 void kmain(int hardt_id, void* device_tree) {
-    disable_interrupts();
-    printf("++++++++++++++++++++++++++++\n");
-    // Will already panic if any extension missing
-    ensure_extensions_present(NECESSARY_RISCV_EXTENSIONS);
-    setup_interrupt_handler();
-    printf("[OK] Booting on on hardware thread %i\n", hardt_id);
-    if (!check_device_tree(device_tree)) {
-        KERNEL_PANIC("Invalid device tree was passed");
+    if (hardt_id == 0) {
+        disable_interrupts();
+        printf("++++++++++++++++++++++++++++\n");
+        // Will already panic if any extension missing
+        ensure_extensions_present(NECESSARY_RISCV_EXTENSIONS);
+        setup_interrupt_handler();
+        printf("[OK] Booting on on hardware thread %i\n", hardt_id);
+        if (!check_device_tree(device_tree)) {
+            KERNEL_PANIC("Invalid device tree was passed");
+        }
+        init_interrupt_controller(device_tree);
+        detect_memory(device_tree);
+        init_phys_allocator();
+        init_power_controller(device_tree);
+        printf("[FINISHED] Nothing to do left: halting\n");
+        /* WARNING: printf, which relies on uart_putchar, is called multiple
+         * times before init_uart. Works in QEMU because I hardcoded the UART
+         * address, but on other platforms, if UART isn't placed at 0x10000000,
+         * then debug messages will simply waste CPU cycles and potentially
+         * cause undefined behavior. Maybe add a preprocessor directive to
+         * indicate whether or not target machine is QEMU virt emulator? */
+        shutdown(0);
+    } else {
+        halt();
     }
-    init_interrupt_controller(device_tree);
-    detect_memory(device_tree);
-    init_phys_allocator();
-    init_power_controller(device_tree);
-    printf("[FINISHED] Nothing to do left: halting\n");
-    /* WARNING: printf, which relies on uart_putchar, is called multiple times
-     * before init_uart. Works in QEMU because I hardcoded the UART address, but
-     * on other platforms, if UART isn't placed at 0x10000000, then debug
-     * messages will simply waste CPU cycles and potentially cause undefined
-     * behavior. Maybe add a preprocessor directive to indicate whether or not
-     * target machine is QEMU virt emulator? */
-    shutdown(0);
 }
